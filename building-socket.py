576626010ad3dba49787be3bf23b0efc00dd471e
PUJAmangale1221 <pujamangale1221@gmail.com>
2024-05-13 14:02:46 +0530

    Building a Socket

--- building-socket.py
+++ building-socket.py
@@ -2,17 +2,27 @@ import socket
 
 import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created successfully")
+except socket.error as err:
+    print(f"socket creation failed with error {err}")
 
 # socket.AF_INET = refers to the address family ipv4
 
 #socket.SOCK_STREAM = refers to TCP protocol //checks connection is established or not
-import socket
+ 
+# hostname = 'www.github.com'
+# ip = socket.gethostbyname(hostname)
+# print(f"The IP address of {hostname} is {ip}")
 
-hostname = 'www.github.com'
-ip = socket.gethostbyname(hostname)
-print(f"The IP address of {hostname} is {ip}")
-
-
-
-
+port = 80
+try:
+    host_ip = socket.gethostbyname("www.github.com")
+except socket.gaierror:
+#gaierror = error with DNS server
+    print("there was an error resolving the host")
+    sys.exit()
+s.connect((host_ip, port))
+print(f"the socket has successfully connected to github on port = {host_ip}")
---
+++ server-client connection/server.py
@@ -0,0 +1,27 @@
+# 1.bind() method which can bind particular IP's and Ports
+# 2. listen() method which allows servers to listen incoming connections
+# 3. accept() accepts connection with a client
+# 4. close() closes connection with a client
+
+
+import socket
+
+s=socket.socket()
+print("socket created successfully")
+
+port=56789
+
+s.bind('', port)
+print(f'socket binded with port {port}')
+
+s.listen(5)
+print('socket is listening')
+
+while True:
+    c, addr = s.accept()
+    print('got connection from', addr)
+    message = ('Thank you for connecting')
+
+    c.send(message.encode())
+    c.close()
+    
